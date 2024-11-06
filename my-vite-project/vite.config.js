53c978d85eab0ad7981bc13ce5014a868f587762
lordbomebob <lordbomebob@gmail.com>
2024-11-06 11:13:47 -0800

    adding dist

--- my-vite-project/vite.config.js
+++
@@ -1,8 +0,0 @@
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  base: 'deploy-my-vite-project/',
-  plugins: [react()],
-})
