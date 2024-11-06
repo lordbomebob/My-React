814821d9b3c110219f1b61d7d97909e7216c49d1
lordbomebob <lordbomebob@gmail.com>
2024-11-06 11:22:46 -0800

    change to base to my-react

--- my-vite-project/vite.config.js
+++ my-vite-project/vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'deploy-my-vite-project/',
+  base: '/My-React/',
   plugins: [react()],
 })
