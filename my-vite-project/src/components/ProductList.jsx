cd2658b2bea55a58bf379fa93be0380ddcd97696
lordbomebob <lordbomebob@gmail.com>
2024-11-06 23:57:01 -0800

    simplify

--- my-vite-project/src/components/ProductList.jsx
+++ my-vite-project/src/components/ProductList.jsx
@@ -1,17 +1,19 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react';
 import { ProductCard } from './ProductCard';
-import { Box } from '@mui/material';
 
 export const ProductList = ({items,addCart}) => {
+  
+    
+
+  
   return (
         <>
         {
           items.length > 0 && items.map(item => {
-              return (<ProductCard name={item.title} category={item.category} price={item.price} img={item.image}  id={item.id}></ProductCard>)
+              return (<ProductCard item={item} ></ProductCard>)
             })
         }
         </>
-        
-
-  )
+    )
 }
+
